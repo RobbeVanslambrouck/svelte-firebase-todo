9264bd7fce5b0ec488a798cef563ebd8036c42c6
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-10-17 23:55:48 +0200

    added header with styling

---
+++ src/lib/Header.svelte
@@ -0,0 +1,23 @@
+<header>
+  <a href="#"><h1>My Todo's</h1></a>
+</header>
+
+<style>
+  header {
+    background-color: #fff;
+    box-shadow: 0 4px 4px 0 rgba(89, 89, 89, 0.25);
+    font-size: 3rem;
+    height: 8rem;
+    line-height: 8rem;
+    text-align: center;
+    text-transform: capitalize;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+</style>
