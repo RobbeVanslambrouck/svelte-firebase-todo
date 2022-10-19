f8a921602a3e27ddaf2ad1e0a4241f98281296ab
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-10-19 23:54:25 +0200

    added header and footer to app

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,25 @@
 <script lang="ts">
+  import Footer from "./lib/Footer.svelte";
+  import Header from "./lib/Header.svelte";
 </script>
 
+<div>
+  <Header />
+  <main>
+    <p>content</p>
+  </main>
+  <Footer />
+</div>
+
 <style>
+  div {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+
+  main {
+    flex: 1;
+    padding: 5rem;
+  }
 </style>
